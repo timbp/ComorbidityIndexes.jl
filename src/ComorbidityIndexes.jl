faa8c2731521debf2d293853b16e71e390adf7c3
Tim Badgery-Parker <tim.badgery-parker@mq.edu.au>
2021-02-02 15:40:10 +1100

    Add Quan Charlson codes

--- src/ComorbidityIndexes.jl
+++ src/ComorbidityIndexes.jl
@@ -3,27 +3,34 @@ module ComorbidityIndexes
 using ICD10Utilities
 
 export QuanElixICD10
+export QuanCCIICD10
 export flagcomorbidities
 
 # comorbidity codes
 include("quan.jl")
 
 # functions
-function flagcomorbidities(A, comorbidity=:quan_elixhauser)
-  if comorbidity == :quan_elixhauser
-    return quanelixhauser(A, QuanElixICD10)
-  elseif comorbidity == :quan_charlson
-  end
-end
 
-function quanelixhauser(icdcodes, comorbcodes)
-  RET = falses(lastindex(comorbcodes))
+"""
+    flagcomorbidities(icdcodes, comorbidities=QuanElixICD10)
+
+Flag comorbidities in an array of ICD-10 codes.
+
+`comorbidities` can be `QuanElixICD10` or `QuanCCIICD10` for Quan versions of
+Elixhauser and Charlson comorbidities, respectively.
+
+Returns NamedTuple giving true or false for each comorbidity.
+
+Weights, summed score, not provided.
+"""
+function flagcomorbidities(icdcodes, comorbidities=QuanElixICD10)
+  RET = falses(lastindex(comorbidities))
   i = 1
-  for cmrb in comorbcodes
-    RET[i] = any(icd -> icd4(icd) in icd4.(cmrb), icdcodes)
+  for cmrb in comorbidities
+    RET[i] = any(icd -> icd3(icd) in icd3.(cmrb[1]), icdcodes) || any(icd -> icd4(icd) in icd4.(cmrb[2]), icdcodes)
     i += 1
   end
-  return eachindex(comorbcodes) .=> RET
+  return (; zip(eachindex(comorbidities), RET)...)
 end
 
 end # module
